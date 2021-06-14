9a1fd4674b1eca2a93c812c2038f01ece317070a
Neo <31198370+GuidoTapia@users.noreply.github.com>
2021-06-14 12:34:18 -0500

    second challenge

--- Front-End/challenge1/script.js
+++ Front-End/challenge1/script.js
@@ -1,1 +1,7 @@
 // Modify this file only
+document.getElementById('increase').onclick = function () {
+    document.getElementById('counter').innerHTML=parseInt(document.getElementById('counter').innerHTML)+1;
+};
+document.getElementById('decrease').onclick = function () {
+    document.getElementById('counter').innerHTML=parseInt(document.getElementById('counter').innerHTML)-1;
+};
